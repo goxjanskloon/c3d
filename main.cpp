992151456ce3049744e530e55a1b65ccc9a73563
goxjanskloon <goxjanskloon@outlook.com>
2023-11-25 23:22:30 +0800

    modified:Use new render3d   main.cpp 	modified:Removed old methods   v3D.cpp 	modified:Use vector3d  v3D.h

--- main.cpp
+++ main.cpp
@@ -2,76 +2,77 @@
 #include<random>
 inline bool keydown(int vKey){ return GetAsyncKeyState(vKey)&0x8000; }
 inline int genint(const int &l,const int &r){
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	return std::uniform_int_distribution<>(l,r)(gen);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    return std::uniform_int_distribution<>(l,r)(gen);
 }
 class render:public render3d{
 public:
-	int focal=400,xl=1000,yl=600,cx=xl>>1,cy=yl>>1;
-	PIMAGE img=newimage(xl,yl);
-	render &init(){
-		initgraph(xl,yl,getinitmode());
-		ege_enable_aa(1,img);
-		setbkmode(TRANSPARENT,img);
-		setfont(20,0,"Consolas",img);
-		setcolor(WHITE,img);
-		return *this;
-	}
-	render &flush(){
-		gendepth().sort_faces().project(focal).render(focal,cx,cy,img);
-		cleardevice();
-		putimage(0,0,img);
-		cleardevice(img);
-		return *this;
-	}
+    vector3d pos{0,0,100},facing{0,0,400},dir{0,1,0};
+    int xl=1000,yl=600,cx=xl>>1,cy=yl>>1;
+    PIMAGE img=newimage(xl,yl);
+    render &init(){
+        initgraph(xl,yl,getinitmode());
+        ege_enable_aa(1,img);
+        setbkmode(TRANSPARENT,img);
+        setfont(20,0,"Consolas",img);
+        setcolor(WHITE,img);
+        return *this;
+    }
+    render &flush(){
+        render3d::render(pos,facing,dir,xl,yl,img);
+        cleardevice();
+        putimage(0,0,img);
+        cleardevice(img);
+        return *this;
+    }
 };
 template<typename... argT>
 inline void xyprintf(const int &x,const int &y,const PIMAGE &img,const char *fmt,argT... args){
-	char bf[8192];
-	sprintf(bf,fmt,args...);
-	outtextxy(x,y,bf,img);
+    char bf[8192];
+    sprintf(bf,fmt,args...);
+    outtextxy(x,y,bf,img);
 }
 void test(){
-	rect3d rect1({-50,50,450},{50,-50,550},{color_t(BLUE),color_t(RED),color_t(YELLOW),color_t(GREEN),color_t(BROWN),color_t(CYAN)}),rect1t=rect1,
-		  rect2({-50,350,450},{50,450,550},{color_t(BLUE),color_t(RED),color_t(YELLOW),color_t(GREEN),color_t(BROWN),color_t(CYAN)}),rect2t=rect2,r1,r2;
-	render rd;
-	render3d_guard rg1(rect1,&rd),rg2(rect2,&rd),rgr1(r1,&rd),rgr2(r2,&rd);
-	rd.init();
-	auto gendeg=[](){return genint(0,359);};
-	auto c1=rect1.center();
-	for(double dx=0,dy=0;;rect1=rect1t,rect2=rect2t){
-		rect1.rotate({0,0,500},gendeg(),gendeg(),gendeg());
-		rect2.rotate({0,0,500},gendeg(),gendeg(),gendeg());
-		auto c2=rect2.center();
-		for(double tx=(c1.x-c2.x)/60,ty=(c1.y-c2.y)/60,tz=(c1.z-c2.z)/60,t=1;t<=120;t++,Sleep(5)){
-			int mx,my;
-			mousepos(&mx,&my);
-			dx=(rd.cy-my)*180.0/rd.cy,dy=(rd.cx-mx)*180.0/rd.cx;
-			rect2.move(tx,ty,tz);
-			auto pb1=rect1.collisionbox(),pb2=rect2.collisionbox();
-			if(keydown(27)) return;
-			if(keydown('C')){
-				r1=rect3d(pb1.first,pb1.second,{EGECOLORA(BLUE,100),EGECOLORA(RED,100),EGECOLORA(YELLOW,100),EGECOLORA(GREEN,100),EGECOLORA(BROWN,100),EGECOLORA(CYAN,100)});
-				r2=rect3d(pb2.first,pb2.second,{EGECOLORA(BLUE,100),EGECOLORA(RED,100),EGECOLORA(YELLOW,100),EGECOLORA(GREEN,100),EGECOLORA(BROWN,100),EGECOLORA(CYAN,100)});
-			}
-			else{
-				r1=rect3d(pb1.first,pb1.second,{EGECOLORA(BLUE,0),EGECOLORA(RED,0),EGECOLORA(YELLOW,0),EGECOLORA(GREEN,0),EGECOLORA(BROWN,0),EGECOLORA(CYAN,0)});
-				r2=rect3d(pb2.first,pb2.second,{EGECOLORA(BLUE,0),EGECOLORA(RED,0),EGECOLORA(YELLOW,0),EGECOLORA(GREEN,0),EGECOLORA(BROWN,0),EGECOLORA(CYAN,0)});
-			}
-			r1.rotate(c1,dx,dy,0),r2.rotate(c1,dx,dy,0);
-			auto t1=rect1,t2=rect2;
-			rect1.rotate(c1,dx,dy,0),rect2.rotate(c1,dx,dy,0);
-			byte c=colliding(pb1,pb2);
-			if(c>>3) outtextxy(0,0,"colliding",rd.img);
-			xyprintf(0,20,rd.img,"X:%d Y:%d Z:%d",c>>2&1,c>>1&1,c>>0&1);
-			xyprintf(0,40,rd.img,"dx=%f dy=%f",dx,dy);
-			rd.flush();
-			rect1=t1,rect2=t2;
-		}
-	}
+    rect3d rect1({-50,50,450},{50,-50,550},{color_t(BLUE),color_t(RED),color_t(YELLOW),color_t(GREEN),color_t(BROWN),color_t(CYAN)}),rect1t=rect1,
+          rect2({-50,350,450},{50,450,550},{color_t(BLUE),color_t(RED),color_t(YELLOW),color_t(GREEN),color_t(BROWN),color_t(CYAN)}),rect2t=rect2,r1,r2;
+    render rd;
+    render3d_guard rg1(rect1,&rd),rg2(rect2,&rd),rgr1(r1,&rd),rgr2(r2,&rd);
+    rd.init();
+    auto gendeg=[](){return genint(0,359);};
+    auto c1=rect1.center();
+    for(double dx=0,dy=0;;rect1=rect1t,rect2=rect2t){
+        rect1.rotate({0,0,500},gendeg(),gendeg(),gendeg());
+        rect2.rotate({0,0,500},gendeg(),gendeg(),gendeg());
+        auto c2=rect2.center();
+        for(double tx=(c1.x-c2.x)/60,ty=(c1.y-c2.y)/60,tz=(c1.z-c2.z)/60,t=1;t<=120;t++,Sleep(5)){
+            int mx,my;
+            mousepos(&mx,&my);
+            dx=(rd.cy-my)*180.0/rd.cy,dy=(rd.cx-mx)*180.0/rd.cx;
+            rect2.move(tx,ty,tz);
+            auto pb1=rect1.collisionbox(),pb2=rect2.collisionbox();
+            if(keydown(27)) return;
+            if(keydown('C')){
+                r1=rect3d(pb1.first,pb1.second,{EGECOLORA(BLUE,100),EGECOLORA(RED,100),EGECOLORA(YELLOW,100),EGECOLORA(GREEN,100),EGECOLORA(BROWN,100),EGECOLORA(CYAN,100)});
+                r2=rect3d(pb2.first,pb2.second,{EGECOLORA(BLUE,100),EGECOLORA(RED,100),EGECOLORA(YELLOW,100),EGECOLORA(GREEN,100),EGECOLORA(BROWN,100),EGECOLORA(CYAN,100)});
+            }
+            else{
+                r1=rect3d(pb1.first,pb1.second,{EGECOLORA(BLUE,0),EGECOLORA(RED,0),EGECOLORA(YELLOW,0),EGECOLORA(GREEN,0),EGECOLORA(BROWN,0),EGECOLORA(CYAN,0)});
+                r2=rect3d(pb2.first,pb2.second,{EGECOLORA(BLUE,0),EGECOLORA(RED,0),EGECOLORA(YELLOW,0),EGECOLORA(GREEN,0),EGECOLORA(BROWN,0),EGECOLORA(CYAN,0)});
+            }
+            r1.rotate(c1,dx,dy,0),r2.rotate(c1,dx,dy,0);
+            auto t1=rect1,t2=rect2;
+            rect1.rotate(c1,dx,dy,0),rect2.rotate(c1,dx,dy,0);
+            byte c=colliding(pb1,pb2);
+            if(c>>3) outtextxy(0,0,"colliding",rd.img);
+            xyprintf(0,20,rd.img,"X:%d Y:%d Z:%d",c>>2&1,c>>1&1,c>>0&1);
+            xyprintf(0,40,rd.img,"dx=%f dy=%f",dx,dy);
+            rd.flush();
+            rect1=t1,rect2=t2;
+        }
+    }
 }
 int main(){
-	test();
-	return 0;
+    test();
+    return 0;
 }
--- v3D.h
+++ v3D.h
@@ -1,7 +1,7 @@
 #pragma once
 #include"EGE/graphics.h"
-#include<algorithm>
 #include<utility>
+#include<cstddef>
 #include<array>
 #include<cmath>
 #include<list>
@@ -9,103 +9,86 @@
 inline double sind(const double &a){ return sin(0.017453292519943295*a); }
 inline double cosd(const double &a){ return cos(0.017453292519943295*a); }
 class vector3d{
-	double x,y,z;
-	vector3d operator*(const double &a){return {x*a,y*a,z*a};}
-	vector3d &operator*=(const double &a){x*=a,y*=a,z*=a;return *this;}
-	double operator*(const vector3d &v){return x*v.x+y*v.y+z*v.z;}
-	vector3d operator&(const vector3d &v){
-		return {y*v.z-v.y*z,v.x*z-z*v.z,x*v.y-v.x*y};
-	}
-	vector3d &operator&=(const vector3d &v){
-		x=y*v.z-v.y*z,y=v.x*z-z*v.z,z=x*v.y-v.x*y;
-		return *this;
-	}
-	double(){return sqrt(x+y+z);}
-
+public:
+    double x,y,z;
+    vector3d operator+(const double &v)const{return {x+v,y+v,z+v};}
+    vector3d &operator+=(const double &v){x+=v,y+=v,z+=v;return *this;}
+    vector3d operator-(const double &v)const{return {x-v,y-v,z-v};}
+    vector3d &operator-=(const double &v){x-=v,y-=v,z-=v;return *this;}
+    vector3d operator+(const vector3d &v)const{return {x+v.x,y+v.y,z+v.z};}
+    vector3d &operator+=(const vector3d &v){x+=v.x,y+=v.y,z+=v.z;return *this;}
+    vector3d operator-(const vector3d &v)const{return {x-v.x,y-v.y,z-v.z};}
+    vector3d &operator-=(const vector3d &v){x-=v.x,y-=v.y,z-=v.z;return *this;}
+    vector3d operator*(const double &a)const{return {x*a,y*a,z*a};}
+    vector3d &operator*=(const double &a){x*=a,y*=a,z*=a;return *this;}
+    vector3d operator/(const double &a)const{return {x/a,y/a,z/a};}
+    vector3d &operator/=(const double &a){x/=a,y/=a,z/=a;return *this;}
+    double operator*(const vector3d &v)const{return x*v.x+y*v.y+z*v.z;}
+    vector3d operator&(const vector3d &v)const{return {y*v.z-v.y*z,v.x*z-z*v.z,x*v.y-v.x*y};}
+    vector3d &operator&=(const vector3d &v){x=y*v.z-v.y*z,y=v.x*z-z*v.z,z=x*v.y-v.x*y;return *this;}
+    vector3d &vector3d::rotate(const vector3d &c,const double &dx,const double &dy,const double &dz);
+    double norm()const{return sqrt(x+y+z);}
 };
 template<typename objT,typename ctrT=std::list<objT>>
 class contnr3d:public ctrT{
 public:
-	double depth=0;
-	contnr3d(){}
-	contnr3d(const ctrT &objs_):ctrT(objs_){}
-	contnr3d &move(const double &tx,const double &ty,const double &tz){
-		for(auto &p:*this) p.move(tx,ty,tz);
-		return *this;
-	}
-	contnr3d &scale(const point3d &c,const double &sx,const double &sy,const double &sz){
-		for(auto &p:*this) p.scale(c,sx,sy,sz);
-		return *this;
-	}
-	contnr3d &rotate(const point3d &c,const double &dx,const double &dy,const double &dz){
-		for(auto &p:*this) p.rotate(c,dx,dy,dz);
-		return *this;
-	}
-	vector3d center()const{
-		vector3d c(0,0,0);
-		for(const auto &p:*this){
-			auto b=p.center();
-			c.move(b.x,b.y,b.z);
-		}
-		const int l=this->size();
-		return {c.x/l,c.y/l,c.z/l};
-	}
-	contnr3d &gendepth(){
-		auto c=center();
-		depth=sqrt(c.x*c.x+c.y*c.y+c.z*c.z);
-		return *this;
-	}
+    contnr3d(){}
+    contnr3d(const ctrT &objs_):ctrT(objs_){}
+    contnr3d &move(const double &tx,const double &ty,const double &tz){
+        for(auto &p:*this) p.move(tx,ty,tz);
+        return *this;
+    }
+    contnr3d &scale(const vector3d &c,const double &sx,const double &sy,const double &sz){
+        for(auto &p:*this) p.scale(c,sx,sy,sz);
+        return *this;
+    }
+    contnr3d &rotate(const vector3d &c,const double &dx,const double &dy,const double &dz){
+        for(auto &p:*this) p.rotate(c,dx,dy,dz);
+        return *this;
+    }
+    vector3d center()const{
+        vector3d c{0,0,0};
+        for(const auto &p:*this) c+=p.center();
+        return c/this->size();
+    }
 };
-class face3d:public contnr3d<point3d>{
+class face3d:public contnr3d<vector3d>{
 public:
-	color_t color=0;
-	face3d(){}
-	face3d(const std::list<point3d> &faces,const color_t &color_):contnr3d(faces),color(color_){}
-	void render(const int &focal,const int &cx,const int &cy,const PIMAGE &img)const;
+    color_t color=0;
+    face3d(){}
+    face3d(const std::list<vector3d> &faces,const color_t &color_):contnr3d(faces),color(color_){}
 };
 class render3d:public std::list<face3d*>{
 public:
-	render3d &project(const int &focal){
-		for(auto &p:*this) p->project(focal);
-		return *this;
-	}
-	render3d &gendepth(){
-		for(auto &p:*this) p->gendepth();
-		return *this;
-	}
-	render3d &sort_faces(){
-		sort([](const face3d *const &a,const face3d *const &b){return a->depth>b->depth;});
-		return *this;
-	}
-	void render(const int &focal,const int &cx,const int &cy,const PIMAGE &img)const{
-		for(const auto &p:*this) p->render(focal,cx,cy,img);
-	}
+    void render(const vector3d &pos,const vector3d &facing,const vector3d &dir,const int &width,const int &height,const PIMAGE &pimg);
 };
 class rect3d:public contnr3d<face3d,std::array<face3d,6>>{
 public:
-	rect3d(){}
-	rect3d(const point3d &a,const point3d &b,const color_t(&colors)[6]);
-	std::pair<point3d,point3d> collisionbox()const;
+    rect3d(){}
+    rect3d(const vector3d &a,const vector3d &b,const color_t(&colors)[6]);
+    std::pair<vector3d,vector3d> collisionbox()const;
 };
 class render3d_guard{
 public:
-	render3d *rd;
-	std::list<std::list<face3d*>::iterator> fp;
-	bool face_removed=0;
-	template<typename ctrT>
-	render3d_guard(contnr3d<face3d,ctrT> &ctr,render3d *const&rd_):rd(rd_){
-		for(int i=0;i<6;i++) rd->push_back(&ctr[i]),fp.emplace_back(prev(rd->end()));
-	}
-	~render3d_guard(){
-		if(!face_removed)
-			for(auto &p:fp) rd->erase(p);
-	}
-	void remove(){
-		if(!face_removed){
-			face_removed=1;
-			for(auto &p:fp) rd->erase(p);
-			fp.erase(fp.begin(),fp.end());
-		}
-	}
+    render3d *cmr;
+    std::list<std::list<face3d*>::iterator> fp;
+    bool face_removed=0;
+    template<typename ctrT>
+    render3d_guard(const contnr3d<face3d,ctrT> &ctr,render3d *const& cmr_):cmr(cmr){
+        for(auto &p:ctr) cmr->push_back(&ctr[i]),fp.emplace_back(prev(rd->end()));
+    }
+    ~render3d_guard(){
+        if(!face_removed)
+            for(auto &p:fp) cmr->erase(p);
+    }
+    bool remove(){
+        if(!face_removed){
+            face_removed=1;
+            for(auto &p:fp) cmr->erase(p);
+            fp.erase(fp.begin(),fp.end());
+            return 1;
+        }
+        return 0;
+    }
 };
-byte colliding(const std::pair<point3d,point3d> &r1,const std::pair<point3d,point3d> &r2);
+byte colliding(const std::pair<vector3d,vector3d> &r1,const std::pair<vector3d,vector3d> &r2);
