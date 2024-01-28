18bb465885d340136ac249d9b84e1f4634881cb6
goxjanskloon <goxjanskloon@outlook.com>
2024-01-28 11:37:53 +0800

    Changing MT policy

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 class render:public renderer3d{
 public:
     vector3d pos{0,0,0},facing{0,0,400},ud{0,1,0},rd{1,0,0};
-    int xl=1000,yl=600,cx=xl>>1,cy=yl>>1;
+    int xl=500,yl=300,cx=xl>>1,cy=yl>>1;
     PIMAGE img=newimage(xl,yl);
     render &init(){
         initgraph(xl,yl,getinitmode());
@@ -15,14 +15,15 @@ public:
         cleardevice(img);
         return *this;
     }
+    ~render(){closegraph();}
 };
 void test(){
     rect3d rect({-50,50,450},{50,-50,550},{color_t(BLUE),color_t(RED),color_t(GREEN),color_t(YELLOW),color_t(BROWN),color_t(CYAN)}),rectt=rect;
     render rd;
     renderer3d_guard rg(rect,&rd);
     rd.init();
-    auto c=rect.center();
-    for(double t=0;is_run();rect=rectt,t+=0.5,Sleep(5)) rect.rotate(c,t,t,t),rd.flush();
+    const auto c=rect.center();
+    for(double t=0;is_run();rect=rectt,t+=0.5,Sleep(1)) rect.rotate(c,t,t,t),rd.flush();
 }
 int main(){
     test();
--- v3D.cpp
+++ v3D.cpp
@@ -1,5 +1,4 @@
 #include"v3D.h"
-#include<iostream>
 #include<utility>
 #include<future>
 #include<limits>
@@ -21,20 +20,20 @@ vector3d &vector3d::rotate(const vector3d &c,const double &dx,const double &dy,c
 void renderer3d::render(const vector3d &pos,const vector3d &facing,const vector3d &ud,const vector3d &rd,const int &width,const int &height,const PIMAGE &pimg){
     auto mid=pos+facing;
     const int hh=height>>1,hw=width>>1;
-    auto f=[](const int &i,const int &j,const renderer3d &renderer,const vector3d &pos,const vector3d &facing,const vector3d &ud,const vector3d &rd,const int &hh,const int &hw,const PIMAGE &pimg){
+    auto f=[&](const int &lx,const int &rx,const int &ly,const int &ry){
+    for(int i=ly;i<ry;i++)for(int j=lx;j<rx;j++){
         std::list<std::pair<double,color_t>> px;
-        for(auto &fp:renderer){
+        for(auto &fp:*this){
             auto &f=*fp;
             const auto e1=f[1]-f[0],e2=f[2]-f[0],d=facing+ud*(hh-i)+rd*(j-hw),pvec=d&e2;
             double det=e1*pvec;
-            if(fabs(det)<std::numeric_limits<double>::epsilon()) continue;
+            if(fabs(det)<=__DBL_EPSILON__) continue;
             det=1/det;
             const auto tvec=pos-f[0];
             const double u=tvec*pvec*det;
             if(u<0||u>1) continue;
             const auto qvec=tvec&e1;
-            const double v=d*qvec*det;
-            std::cout<<v<<' '<<u+v<<'\n';
+            const double v=(d*qvec)*det;
             if(v<0||u+v>1) continue;
             const double t=e2*qvec*det;
             if(t>0) px.emplace_back(t,f.color);
@@ -43,10 +42,11 @@ void renderer3d::render(const vector3d &pos,const vector3d &facing,const vector3
         for(auto &p:px){
             putpixel_f(j,i,p.second,pimg);
             if(EGEGET_A(p.second)==0xff) break;
-        }
-    };
-    for(int i=0;i<height;i++)
-        for(int j=0;j<width;j++) std::async(std::launch::async|std::launch::deferred,f,i,j,*this,pos,facing,ud,rd,hh,hw,pimg);
+    }}};
+    std::async(std::launch::async,f,0,hw,0,hh),
+    std::async(std::launch::async,f,hw,width,0,hh),
+    std::async(std::launch::async,f,0,hw,hh,height),
+    std::async(std::launch::async,f,hw,width,hh,height);
 }
 rect3d::rect3d(const vector3d &a,const vector3d &b,const color_t(&colors)[6]):contnr3d({
     triface3d({b.x,a.y,a.z},{a.x,b.y,a.z},{a.x,a.y,a.z},colors[0]),
