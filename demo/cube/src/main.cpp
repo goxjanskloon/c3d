b3e81f90bcf40ff974e22298b035e013dbe487fe
goxjanskloon <goxjanskloon@outlook.com>
2024-10-06 23:02:36 +0800

    In progress of fixing coloring

--- demo/cube/src/main.cpp
+++ demo/cube/src/main.cpp
@@ -3,33 +3,28 @@
 #include<cstring>
 #include<fstream>
 #include<iostream>
-#include<vector>
-#include"v3d.hpp"
-class ground_t:public v3d::renderable{
-    static constexpr v3d::vector e1{1.0,0.0,0.0},e2{0.0,0.0,1.0},normal{0.0,1.0,0.0};
+#include<v3d.hpp>
+using namespace v3d;
+class Ground:public Renderable{
+    static constexpr Vector e1{1.0,0.0,0.0},e2{0.0,0.0,1.0},normal{0.0,1.0,0.0};
 public:
-    double y,roughn;
-    v3d::light_t light;
-    ground_t(const double &y,const v3d::light_t &light,const double &roughn):y(y),light(light),roughn(roughn){}
-    virtual std::shared_ptr<pickpoint_t> pick(const v3d::vector &pos,const v3d::vector &ray)const override{
-        double det=e1*(ray&e2);
+    Real y,roughn;
+    Color color;
+    Ground(CR<Real> y,CR<Real> roughn,CR<Color> color):y(y),roughn(roughn),color(color){}
+    Ptr<Point> pick(CR<Ray> ray)const override{
+        double det=e1*(ray.ray&e2);
         if(fabs(det)<v3d::EPSILON) return nullptr;
-        if(const double t=e2*((pos-v3d::vector{0.0,y,0.0})&e1)/det;t>v3d::EPSILON) return std::make_shared<pickpoint_t>(pos+ray*t,normal,light,t,roughn);
+        if(const double t=e2*((ray.pos-Vector{0.0,y,0.0})&e1)/det;t>EPSILON) return makePtr<Point>(ray.at(t),normal,color,t,roughn);
         return nullptr;
     }
 };
-unsigned int d2c(const double &x){return std::min<unsigned int>(0xffu,x);}
+unsigned int d2c(CR<Real> x){return std::min<unsigned int>(255u,x);}
 int main(){
-    constexpr int WIDTH=1000,HEIGHT=600,PGBAR_LEN=100;
-    constexpr v3d::color_t BLUE{0.0,0.0,255.0},RED{255.0,0.0,0.0},YELLOW{0.0,255.0,255.0},GREEN{0.0,255.0,0.0},BROWN{165.0,42.0,42.0},CYAN{0.0,255.0,255.0},BLACK,WHITE{255.0,255.0,255.0};
-    v3d::light_t colors[]{{BLUE,0.0},{RED,0.0},{YELLOW,0.0},{GREEN,0.0},{BROWN,0.0},{CYAN,0.0}};
-    v3d::rect rect({-5.0,5.0,-5.0},{5.0,-5.0,5.0},colors,0.9);
-    rect.rotate({0.0,0.0,0.0},{0.0,1.0,0.0},M_PI_4*1.2);
-    rect.rotate({0.0,0.0,0.0},{1.0,0.0,0.0},-M_PI_4*0.7);
-    v3d::renderer renderer({-5.0,-2.0,-20.0},{0.0,0.0,300.0},{0.0,1.0,0.0},{1.0,0.0,0.0},WIDTH,HEIGHT,{WHITE,0.01});
-    v3d::renderer_guard rgr(rect,&renderer);
-    renderer.emplace_back(new v3d::sphere({-12.0,0.0,-3.0},5,{RED,0.1},0.9));
-    renderer.emplace_back(new ground_t(-9.0,{CYAN,0.0},1.0));
+    constexpr UInt WIDTH=1000u,HEIGHT=600u,PGBAR_LEN=100u;
+    constexpr Color BLUE{0.0,0.0,255.0},RED{255.0,0.0,0.0},YELLOW{0.0,255.0,255.0},GREEN{0.0,255.0,0.0},BROWN{165.0,42.0,42.0},CYAN{0.0,255.0,255.0},WHITE{255.0,255.0,255.0};
+    Renderer renderer({{0,0,-20.0},{0.0,0.0,300.0}},{0.0,1.0,0.0},{1.0,0.0,0.0},WIDTH,HEIGHT,WHITE*0.2);
+    //renderer.emplace_back(new Sphere({-12.0,0.0,0.0},5.0,RED,0.9));
+    renderer.emplace_back(new Ground(-10.0,1.0,CYAN));
     std::ofstream image("cube.ppm");
     image<<"P3\n"<<WIDTH<<' '<<HEIGHT<<"\n255\n";
     for(int i=0,l=0;i<HEIGHT;++i)
--- include/v3d.hpp
+++ include/v3d.hpp
@@ -1,129 +1,124 @@
 #pragma once
-#ifdef _MSC_VER
-#define NOMINMAX
-#endif
 #include<algorithm>
 #include<array>
 #include<cfloat>
 #include<cmath>
 #include<list>
 #include<memory>
-#include<vector>
 namespace v3d{
-    constexpr double EPSILON=1e-5;
-    class vector{
+    using Real=double;
+    using Int=int;
+    using UInt=unsigned int;
+    template<typename E> using Ptr=std::shared_ptr<E>;
+    template<typename E> using List=std::list<E>;
+    template<typename E,UInt L> using Array=std::array<E,L>;
+    template<typename T> using CR=const T&;
+    constexpr Real EPSILON=1e-10;
+    template<typename E,typename... A> Ptr<E> makePtr(A&&... args){return std::make_shared<E>(args...);}
+    class Ray;
+    class Vector{
     public:
-        double x,y,z;
-        constexpr vector():x(0.0),y(0.0),z(0.0){};
-        constexpr vector(const double &x,const double &y,const double &z):x(x),y(y),z(z){}
-        vector operator+(const vector &v)const{return {x+v.x,y+v.y,z+v.z};}
-        vector &operator+=(const vector &v){x+=v.x,y+=v.y,z+=v.z;return *this;}
-        vector operator-(const vector &v)const{return {x-v.x,y-v.y,z-v.z};}
-        vector &operator-=(const vector &v){x-=v.x,y-=v.y,z-=v.z;return *this;}
-        vector operator*(const double &a)const{return {x*a,y*a,z*a};}
-        vector &operator*=(const double &a){x*=a,y*=a,z*=a;return *this;}
-        double operator*(const vector &v)const{return x*v.x+y*v.y+z*v.z;}
-        vector operator/(const double &a)const{return {x/a,y/a,z/a};}
-        vector &operator/=(const double &a){x/=a,y/=a,z/=a;return *this;}
-        vector operator&(const vector &v)const{return {y*v.z-z*v.y,z*v.x-x*v.z,x*v.y-y*v.x};}
-        vector &rotate(const vector &base,const vector &axis,const double &angle){
-            *this-=base;
-            const double cosa=cos(angle);
-            return *this=*this*cosa+axis*(1-cosa)*(axis**this)+(axis*sin(angle)&*this)+base;
-        }
-        double norm()const{return sqrt(x*x+y*y+z*z);}
-        vector unit()const{return operator/(norm());}
-        vector &unitize(){return operator/=(norm());}
+        Real x,y,z;
+        constexpr Vector():x(0.0),y(0.0),z(0.0){};
+        constexpr Vector(CR<Real> x,CR<Real> y,CR<Real> z):x(x),y(y),z(z){}
+        Vector operator+(CR<Vector> v)const{return {x+v.x,y+v.y,z+v.z};}
+        Vector &operator+=(CR<Vector> v){x+=v.x,y+=v.y,z+=v.z;return *this;}
+        Vector operator-(CR<Vector> v)const{return {x-v.x,y-v.y,z-v.z};}
+        Vector &operator-=(CR<Vector> v){x-=v.x,y-=v.y,z-=v.z;return *this;}
+        Vector operator*(CR<Real> a)const{return {x*a,y*a,z*a};}
+        Vector &operator*=(CR<Real> a){x*=a,y*=a,z*=a;return *this;}
+        Real operator*(CR<Vector> v)const{return x*v.x+y*v.y+z*v.z;}
+        Vector operator/(CR<Real> a)const{return {x/a,y/a,z/a};}
+        Vector &operator/=(CR<Real> a){x/=a,y/=a,z/=a;return *this;}
+        Vector operator&(CR<Vector> v)const{return {y*v.z-z*v.y,z*v.x-x*v.z,x*v.y-y*v.x};}
+        Vector &rotate(CR<Ray> axis,CR<Real> angle);
+        Real norm()const{return sqrt(x*x+y*y+z*z);}
+        Vector unit()const{return operator/(norm());}
+        Vector &unitize(){return operator/=(norm());}
     };
-    using color_t=vector;
-    struct light_t{
-        color_t color;
-        double brightn;
-        light_t():color(),brightn(0.0){};
-        light_t(const color_t &color,const double &brightn):color(color),brightn(brightn){}
-        light_t operator*(const double &rate){return {color,brightn*rate};}
+    using Color=Vector;
+    class Ray{
+    public:
+        Vector pos,ray;
+        Vector at(CR<Real> t)const{return pos+ray*t;}
     };
-    light_t blend(const std::initializer_list<light_t> &light){
-        light_t res;
-        for(auto &p:light) res.brightn+=p.brightn,res.color+=p.color*p.brightn;
-        res.color/=res.brightn;
-        return res;
+    Vector &Vector::rotate(CR<Ray> axis,CR<Real> angle){
+        *this-=axis.ray;
+        const Real cosa=cos(angle);
+        return *this=*this*cosa+axis.ray*(1-cosa)*(axis.ray**this)+(axis.ray*sin(angle)&*this)+axis.pos;
     }
-    template<typename objT,typename ctrT=std::list<objT>> class collection:public ctrT{
+    template<typename E,typename C=std::list<E>> class Collection:public C{
     public:
-        collection()=default;
-        explicit collection(const ctrT &objs_):ctrT(objs_){}
-        collection &rotate(const vector &base,const vector &axis,const double &angle){
+        Collection()=default;
+        explicit Collection(const C &objs):C(objs){}
+        Collection &rotate(CR<Vector> base,CR<Vector> axis,CR<Real> angle){
             for(auto &p:*this) p.rotate(base,axis,angle);
             return *this;
         }
     };
-    class renderable{
+    class Renderable{
     public:
-        struct pickpoint_t{
-            vector point,normal;
-            light_t light;
-            double dist,roughn;
-            
+        class Point{
+        public:
+            Vector point,normal;
+            Color color;
+            Real dist,roughn;
         };
-        virtual std::shared_ptr<pickpoint_t> pick(const vector &pos,const vector &ray)const=0;
+        virtual ~Renderable()=default;
+        virtual Ptr<Point> pick(CR<Ray> ray)const=0;
     };
-    class renderer:public std::list<const renderable*>{
+    class Renderer:public List<Ptr<const Renderable>>{
     public:
-        static constexpr unsigned int SSAA_SIZE=3;
-        static constexpr unsigned int SSAA_COUNT=SSAA_SIZE*SSAA_SIZE;
-        static constexpr double SSAA_OFFSET[SSAA_SIZE]{-0.33,0.0,0.33};
-        vector pos,facing,ud,rd;
-        int width,height;
-        light_t bglight;
-        renderer(const vector &pos,const vector &facing,const vector &ud,const vector &rd,const int &width,const int &height,const light_t &bglight):pos(pos),facing(facing),ud(ud),rd(rd),width(width),height(height),bglight(bglight){}
-        light_t render(const vector &pos,const vector &ray,const int &rtd)const{
-            using pdc=std::pair<double,color_t>;
-            std::shared_ptr<renderable::pickpoint_t> point(nullptr);
+        static constexpr UInt SSAA_SIZE=3,SSAA_COUNT=SSAA_SIZE*SSAA_SIZE;
+        static constexpr Real SSAA_OFFSET[SSAA_SIZE]{-0.33,0.0,0.33};
+        Vector ud,rd;
+        Ray ray;
+        UInt width,height;
+        Color bgcolor;
+        Renderer(CR<Ray> pos,CR<Vector> ud,CR<Vector> rd,CR<UInt> width,CR<UInt> height,CR<Color> bgcolor):ray(ray),ud(ud),rd(rd),width(width),height(height),bgcolor(bgcolor){}
+        Color render(CR<Ray> ray,CR<UInt> rtd)const{
+            Ptr<const Renderable::Point> point(nullptr);
             for(const auto &fp:*this)
-                if(const auto t=fp->pick(pos,ray);t.get()!=nullptr&&(point.get()==nullptr||t->dist<point->dist)) point=t;
-            if(point.get()==nullptr) return bglight;
-            const auto &[cpoint,normal,light,dist,roughn]=*point.get();
-            if(rtd) return blend({light,bglight,render(cpoint,ray-normal*(ray*normal)*2,rtd-1)*std::min(1-roughn,(ray*-1)*normal)});
-            return blend({light,bglight});
+                if(const auto t=fp->pick(ray);t.get()!=nullptr&&(point.get()==nullptr||t->dist<point->dist)) point=t;
+            if(point.get()==nullptr) return {};
+            const auto &[cpoint,normal,color,dist,roughn]=*point.get();
+            if(rtd) return bgcolor+color*roughn+render({cpoint,ray.ray-normal*(ray.ray*normal)*2},rtd-1)*((ray.ray*-1.0)*normal)*(1.0-roughn);
+            return bgcolor+color*roughn;
         }
-        color_t render_ssaa(const int &x,const int &y,const int &rtd)const{
-            light_t res;
-            const int hh=height>>1,hw=width>>1;
-            for(int i=0;i<SSAA_SIZE;++i)
-                for(int j=0;j<SSAA_SIZE;++j){
-                    const auto c=render(pos,(facing+ud*(hh-y+SSAA_OFFSET[i])+rd*(x-hw+SSAA_OFFSET[j])).unit(),rtd);
-                    res.color+=c.color,res.brightn+=c.brightn;
-                }
-            return res.color/SSAA_COUNT*res.brightn/SSAA_COUNT;
+        Color render_ssaa(CR<UInt> x,CR<UInt> y,CR<UInt> rtd)const{
+            Color res;
+            const auto hh=height>>1,hw=width>>1;
+            for(UInt i=0;i<SSAA_SIZE;++i)
+                for(UInt j=0;j<SSAA_SIZE;++j)
+                    res+=render({ray.pos,(ray.ray+ud*(hh-y+SSAA_OFFSET[i])+rd*(x-hw+SSAA_OFFSET[j])).unit()},rtd);
+            return res/=SSAA_COUNT;
         }
-        color_t render(const int &x,const int &y,const int &rtd)const{
-            const auto t=render(pos,(facing+ud*((height>>1)-y)+rd*(x-(width>>1))).unit(),rtd);
-            return t.color/t.brightn;
+        Color render(CR<UInt> x,CR<UInt> y,CR<UInt> rtd)const{
+            return render({ray.pos,(ray.ray+ud*((height>>1)-y)+rd*(x-(width>>1))).unit()},rtd);
         }
     };
-    class triface:public collection<vector,std::array<vector,3>>,public renderable{
+    /*class TriFace:public Collection<Vector,Array<Vector,3>>,public Renderable{
     public:
-        light_t light;
-        double roughn;
-        triface(const vector &v1,const vector &v2,const vector &v3,const light_t &light,const double &roughn):collection({v1,v2,v3}),light(light),roughn(roughn){}
-        virtual std::shared_ptr<pickpoint_t> pick(const vector&pos,const vector&ray)const override{
+        Color color;
+        Real roughn;
+        triface(const vector &v1,const vector &v2,const vector &v3,const light_t &light,CR<Real> roughn):collection({v1,v2,v3}),light(light),roughn(roughn){}
+        std::shared_ptr<pickpoint_t> pick(const vector&pos,const vector&ray)const override{
             const auto e1=at(1)-at(0),e2=at(2)-at(0),pv=ray&e2;
-            double det=e1*pv;
+            Real det=e1*pv;
             if(fabs(det)<EPSILON) return nullptr;
             det=1/det;
             const auto tvec=pos-at(0);
-            const double u=tvec*pv*det;
+            const Real u=tvec*pv*det;
             if(u<0||u>1) return nullptr;
             const auto qv=tvec&e1;
-            if(const double v=ray*qv*det;v<0||u+v>1) return nullptr;
-            if(const double t=e2*qv*det;t>EPSILON) return std::make_shared<pickpoint_t>(pos+ray*t,(e1&e2).unit(),light,t,roughn);
+            if(const Real v=ray*qv*det;v<0||u+v>1) return nullptr;
+            if(const Real t=e2*qv*det;t>EPSILON) return std::make_shared<pickpoint_t>(pos+ray*t,(e1&e2).unit(),light,t,roughn);
             return nullptr;
         }
     };
     class rect:public collection<triface,std::array<triface,12>>{
     public:
-        template<typename arrT> rect(const vector &a,const vector &b,const arrT &colors,const double &roughn):collection({
+        template<typename arrT> rect(const vector &a,const vector &b,const arrT &colors,CR<Real> roughn):collection({
             triface({b.x,a.y,a.z},{a.x,b.y,a.z},a,colors[0],roughn),
             triface({b.x,a.y,a.z},{a.x,b.y,a.z},{b.x,b.y,a.z},colors[0],roughn),
             triface({b.x,a.y,b.z},{a.x,b.y,b.z},{a.x,a.y,b.z},colors[1],roughn),
@@ -136,22 +131,22 @@ namespace v3d{
             triface({b.x,a.y,b.z},a,{b.x,a.y,a.z},colors[4],roughn),
             triface(b,{a.x,b.y,a.z},{a.x,b.y,b.z},colors[5],roughn),
             triface(b,{a.x,b.y,a.z},{b.x,b.y,a.z},colors[5],roughn)}){}
-    };
-    class sphere:public renderable{
+    };*/
+    class Sphere:public Renderable{
     public:
-        vector center;
-        double radius,roughn,brightn;
-        light_t light;
-        sphere(const vector &center,const double &radius,const light_t &light,const double &roughn):center(center),radius(radius),light(light),roughn(roughn){}
-        virtual std::shared_ptr<pickpoint_t> pick(const vector &pos,const vector &ray)const override{
-            const auto cp=(pos-center);
-            const double b=ray*cp,d=b*b-cp*cp+radius*radius;
+        Vector center;
+        Real radius,roughn;
+        Color color;
+        Sphere(CR<Vector> center,CR<Real> radius,CR<Color> color,CR<Real> roughn):center(center),radius(radius),color(color),roughn(roughn){}
+        Ptr<Point> pick(CR<Ray> ray)const override{
+            const auto cp=(ray.pos-center);
+            const Real b=ray.ray*cp,d=b*b-cp*cp+radius*radius;
             if(d<0) return nullptr;
-            if(const double t=-b-sqrt(d);t>EPSILON) return std::make_shared<pickpoint_t>(pos+ray*t,(cp+ray*t).unit(),light,t,roughn);
+            if(const Real t=-b-sqrt(d);t>EPSILON) return makePtr<Point>(ray.at(t),(cp+ray.ray*t).unit(),color,t,roughn);
             return nullptr;
         }
     };
-    class renderer_guard{
+    /*class renderer_guard{
     public:
         renderer *const rd;
         std::list<std::list<const renderable*>::iterator> fp;
@@ -173,5 +168,5 @@ namespace v3d{
             }
             return false;
         }
-    };
+    };*/
 }
