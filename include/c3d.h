ed879d75214a6a4ffad16bc4309166579f1868ab
goxjanskloon <goxjanskloon@outlook.com>
2025-05-04 14:25:35 +0800

    _

--- include/c3d.h
+++ include/c3d.h
@@ -7,8 +7,7 @@
 #include<ranges>
 namespace c3d{
     constexpr float INF=std::numeric_limits<float>::infinity(),PI=std::acosf(-1);
-    class interval{
-    public:
+    struct interval{
         float min,max;
         [[nodiscard]] float clamp(const float a)const{
             return a<min?min:a>max?max:a;
@@ -43,8 +42,7 @@ namespace c3d{
     inline interval intersect(const interval &a,const interval &b){
         return{std::max(a.min,b.min),std::min(a.max,b.max)};
     }
-    class vector{
-    public:
+    struct vector{
         float x,y,z;
         void rotate(const vector &origin,const vector &axis,float a);
         void rotate(const vector &axis,float a);
@@ -120,23 +118,22 @@ namespace c3d{
         const vector v{std::cos(l)*r,std::sin(l)*r,1-2*b};
         return v*n>0?v:-v;
     }
-    class ray{
-    public:
+    struct ray{
         vector origin,direction;
         [[nodiscard]] vector at(const float a)const{
             return origin+direction*a;
         }
     };
-    class aabb{
-    public:
+    struct aabb{
         interval x,y,z;
+        aabb()=default;
         aabb(const interval &x,const interval &y,const interval &z):x(x),y(y),z(z){}
         aabb(const vector &a,const vector &b):x{std::min(a.x,b.x),std::max(a.x,b.x)},y{std::min(a.y,b.y),std::max(a.y,b.y)},z{std::min(a.z,b.z),std::max(a.z,b.z)}{}
         aabb(const aabb &a,const aabb &b):x(c3d::unite(a.x,b.x)),y(c3d::unite(a.y,b.y)),z(c3d::unite(a.z,b.z)){}
-        [[nodiscard]] bool hit(const vector &origin,const vector &ray,interval interval)const{
-            return !(interval.intersect({(x.min-origin.x)/ray.x,(x.max-origin.x)/ray.x}).empty()
-                   ||interval.intersect({(y.min-origin.y)/ray.y,(y.max-origin.y)/ray.y}).empty()
-                   ||interval.intersect({(z.min-origin.z)/ray.z,(z.max-origin.z)/ray.z}).empty());
+        [[nodiscard]] bool hit(const vector &origin,const vector &ray,interval interval_)const{
+            return !(interval_.intersect({(x.min-origin.x)/ray.x,(x.max-origin.x)/ray.x}).empty()
+                   ||interval_.intersect({(y.min-origin.y)/ray.y,(y.max-origin.y)/ray.y}).empty()
+                   ||interval_.intersect({(z.min-origin.z)/ray.z,(z.max-origin.z)/ray.z}).empty());
         }
         void unite(const aabb &a){
             x.unite(a.x);
@@ -153,14 +150,14 @@ namespace c3d{
     }
     class material;
     struct hit_record{
-        vector point,normal,color;
-        float distance;
+        vector point,normal;
+        float distance,u,v;
         std::shared_ptr<const material> material_;
     };
     class hittable{
     public:
         virtual ~hittable()=0;
-        [[nodiscard]] virtual std::shared_ptr<hit_record> hit(const vector& origin,const vector& ray,const interval& interval)const=0;
+        [[nodiscard]] virtual std::shared_ptr<hit_record> hit(const vector& origin,const vector& ray,const interval& interval_)const=0;
         [[nodiscard]] virtual aabb get_aabb()const=0;
     };
     class pdf{
@@ -180,30 +177,30 @@ namespace c3d{
         virtual vector emit(const ray &in,const hit_record &hit,float u,float v,const vector &p){
             return{.0f,.0f,.0f};
         }
-        [[nodiscard]] virtual std::shared_ptr<scatter_record> scatter(const ray& in,const hit_record &hit)const{
-            return nullptr;
-        }
+        [[nodiscard]] virtual std::shared_ptr<scatter_record> scatter(const ray& in,const hit_record &hit)const=0;
     };
     class bvh_tree final:public hittable{
-        bvh_tree(std::vector<std::shared_ptr<const hittable>> &objects,const int l,const int r){
-            if(const std::size_t n=objects.size();n==1){
+        bvh_tree(std::vector<std::shared_ptr<const hittable>> &objects,const int l,const int r):aabb_(){
+            aabb_=empty_aabb;
+            if(const std::size_t n=objects.size();n==1) {
                 aabb_=(left=objects.front())->get_aabb();
                 right=nullptr;
-            }else if(n==2)
+            } else if(n==2)
                 aabb_={(left=objects.front())->get_aabb(),(right=objects.back())->get_aabb()};
-            else{
+            else {
                 aabb_=objects.front()->get_aabb();
                 for(auto p=std::next(objects.begin());p!=objects.end();++p)
                     aabb_.unite((*p)->get_aabb());
                 const float xl=aabb_.x.length(),yl=aabb_.y.length(),zl=aabb_.z.length();
                 const auto lp=objects.begin()+l,rp=objects.begin()+r;
-                std::sort(std::execution::par_unseq,lp,rp,[&](const std::shared_ptr<const hittable> &a,const std::shared_ptr<const hittable> &b){
-                    if(xl>yl&&xl>zl)
-                        return a->get_aabb().x.min<b->get_aabb().x.min;
-                    if(yl>xl&&yl>zl)
-                        return a->get_aabb().y.min<b->get_aabb().y.min;
-                    return a->get_aabb().z.min<b->get_aabb().z.min;
-                });
+                std::sort(std::execution::par_unseq,lp,rp,
+                          [&](const std::shared_ptr<const hittable>&a,const std::shared_ptr<const hittable>&b) {
+                              if(xl>yl&&xl>zl)
+                                  return a->get_aabb().x.min<b->get_aabb().x.min;
+                              if(yl>xl&&yl>zl)
+                                  return a->get_aabb().y.min<b->get_aabb().y.min;
+                              return a->get_aabb().z.min<b->get_aabb().z.min;
+                          });
                 const int m=l+r>>1;
                 left=std::shared_ptr<hittable>(new bvh_tree(objects,l,m));
                 right=std::shared_ptr<hittable>(new bvh_tree(objects,m+1,r));
@@ -213,33 +210,35 @@ namespace c3d{
         std::shared_ptr<const hittable> left,right;
         aabb aabb_;
         explicit bvh_tree(std::vector<std::shared_ptr<const hittable>> &objects):bvh_tree(objects,0,static_cast<int>(objects.size())-1){}
-        [[nodiscard]] std::shared_ptr<hit_record> hit(const vector &origin,const vector &ray,const interval &interval)const override{
-
+        [[nodiscard]] std::shared_ptr<hit_record> hit(const vector &origin,const vector &ray,const interval &interval_)const override{
+            auto left_hit=left==nullptr?nullptr:left->hit(origin,ray,interval_),right_hit=right==nullptr?nullptr:right->hit(origin,ray,interval_);
+            if(left_hit==nullptr) return right_hit;
+            if(right_hit==nullptr) return left_hit;
+            return left_hit->distance<right_hit->distance?left_hit:right_hit;
         }
         [[nodiscard]] aabb get_aabb()const override{
             return aabb_;
         }
     };
-    class Sphere final:public Hittable{
+    class sphere final:public hittable{
     public:
         vector center;
-        double radius;
-        std::shared_ptr<Light> light;
-        std::shared_ptr<Material> material;
-        [[nodiscard]] std::shared_ptr<HitRecord> hit(const vector& origin,const vector& ray,const interval&interval)const override{
-            //TODO:sync with dev-java
+        float radius;
+        std::shared_ptr<material> material_;
+        [[nodiscard]] std::shared_ptr<hit_record> hit(const vector &origin,const vector &ray,const interval &interval_)const override{
             const vector co=origin-center;
-            const double b=ray*co,d=b*b-normSq(co)+radius*radius;
+            const float b=ray*co,d=b*b-self_dot(co)+radius*radius;
             if(d<0)
                 return nullptr;
-            const double sd=std::sqrt(d);
-            double t=-b-sd;
-            if(t<EPSILON)
+            const float sd=std::sqrt(d);
+            float t=-b-sd;
+            if(!interval_.contain(t)){
                 t+=sd*2;
-            if(t<EPSILON)
-                return nullptr;
+                if(!interval_.contain(t))
+                    return nullptr;
+            }
             const vector point=origin+ray*t;
-            return std::make_shared<HitRecord>(HitRecord{point,(point-center).unitize(),light,t,material});
+            return std::make_shared<hit_record>(hit_record{point,(point-center).unitize(),light,t,material_});
         }
         [[nodiscard]] Aabb aabb()const override{return{{center.x-radius,center.x+radius},{center.y-radius,center.y+radius},{center.z-radius,center.z+radius}};}
     };
